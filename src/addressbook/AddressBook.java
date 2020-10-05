f90afeaa375caf3b6049cb813bbbc9e70eac8899
unknown <ompreetam317@gmail.com>
2020-10-05 19:31:16 +0530

    [Preetam]Added: UC7 Check Duplicates using javastream

---
+++ src/addressbook/AddressBook.java
@@ -0,0 +1,163 @@
+package addressbook;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class AddressBook {
+	private LinkedList<Contacts>  contactList;
+	private LinkedList<String>  fNamelName;
+	
+	Scanner myObj = new Scanner(System.in);
+	
+	public AddressBook() {
+		
+		contactList = new LinkedList<>();
+		fNamelName = new LinkedList<>();
+	}
+	
+	public Contacts addAdress() {
+		
+		System.out.println("Enter First Name");
+		String firstName = myObj.nextLine();
+		
+		
+		
+		
+		System.out.println("Enter Last Name");
+		String lastName = myObj.nextLine();
+		
+		fNamelName.add(firstName+" "+lastName);
+		System.out.println("Enter Address");
+		String Address = myObj.nextLine();
+		System.out.println("Enter City");
+		String City = myObj.nextLine(); 
+		System.out.println("Enter State");
+		String State = myObj.nextLine();
+		System.out.println("Enter zip Code");
+		String zip = myObj.nextLine();
+		System.out.println("Enter Phone Number");
+		String phoneNumber = myObj.nextLine();
+		System.out.println("Enter Email Address");
+		String email = myObj.nextLine();
+		Contacts contacts = new Contacts(firstName,lastName,Address,City,State,zip,phoneNumber,email);
+		contactList.add(contacts);
+		if(findDuplicateBySetAdd(contactList))
+		{
+			System.out.println("Contact Already Exists");
+			return null;
+		}
+		else 
+		{
+			
+			
+			return contacts;
+		}
+		
+		
+		
+	}
+	
+	public void delete() {
+		
+		System.out.println("Enter First Name");
+		String firstName = myObj.nextLine();
+		System.out.println("Enter Last Name");
+		String lastName = myObj.nextLine();
+		
+		
+		for(int i=0;i<contactList.size();i++) {
+			
+			if((contactList.get(i).firstName.equals(firstName) ) && (contactList.get(i).lastName.equals(lastName) )) {
+				
+				contactList.remove(i);				
+				System.out.println("Contact Deleted");
+			}
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	public void displayAllContacts() {
+		
+		for(int i = 0;i<contactList.size();i++) {
+			
+			Contacts e = contactList.get(i);
+			System.out.println("Printing Details for "+contactList.get(i).firstName+ " " + contactList.get(i).lastName);
+			
+			System.out.println(e.Address);
+			System.out.println(e.City);
+			System.out.println(e.State);
+			System.out.println(e.zip);
+			System.out.println(e.phoneNumber);
+			System.out.println(e.email);
+			System.out.println("========================");
+			
+		}
+		
+	}
+	
+	public void editUsingName() {
+		
+		System.out.println("Enter First Name");
+		String firstName = myObj.nextLine();
+		System.out.println("Enter Last Name");
+		String lastName = myObj.nextLine();
+		
+		
+		for(int i=0;i<contactList.size();i++) {
+			
+			if((contactList.get(i).firstName.equals(firstName) ) && (contactList.get(i).lastName.equals(lastName) )) {
+				
+				System.out.println("Enter New First Name");
+				contactList.get(i).firstName = myObj.nextLine();
+				System.out.println("Enter New Last Name");
+				contactList.get(i).lastName = myObj.nextLine();
+				System.out.println("Enter New Address");
+				contactList.get(i).Address = myObj.nextLine();
+				System.out.println("Enter New City");
+				contactList.get(i).City = myObj.nextLine(); 
+				System.out.println("Enter New State");
+				contactList.get(i).State = myObj.nextLine();
+				System.out.println("Enter New zip Code");
+				contactList.get(i).zip = myObj.nextLine();
+				System.out.println("Enter New Phone Number");
+				contactList.get(i).phoneNumber = myObj.nextLine();
+				System.out.println("Enter New Email Address");
+				contactList.get(i).email = myObj.nextLine();
+				
+			}
+			else
+				System.out.println("Entry Not Found");
+			
+		}
+		
+		
+		
+	}
+	
+	public boolean findDuplicateBySetAdd(LinkedList<Contacts> list) {
+
+        Set<Contacts> items = new HashSet<>();
+        return (list.stream()
+                .filter(n -> !items.add(n)) // Set.add() returns false if the element was already in the set.
+                .collect(Collectors.toSet()).size())>=1;
+	}
+	
+	
+
+		
+		
+	}
+	
+	
+	
+
---
+++ src/addressbook/Contacts.java
@@ -0,0 +1,45 @@
+package addressbook;
+
+import java.util.Objects;
+
+public class Contacts {
+	public String firstName;
+	public String lastName;
+	public String Address;
+	public String City;
+	public String State;
+	public String zip;
+	public String phoneNumber;
+	public String email;
+	
+	public Contacts(String firstName,String lastName,String Address,String City, String State,String zip,String phoneNumber,String email) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.Address = Address;
+		this.City = City;
+		this.State = State;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstName, lastName, Address, City, State, zip, phoneNumber, email);
+		
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+	      if (obj == this) {
+	         return true;
+	      }
+	      if (!(obj instanceof Contacts)) {
+	         return false;
+	      }
+	      Contacts c = (Contacts) obj;
+	      return firstName.equals(c.firstName)&& lastName.equals(c.lastName);
+	   }
+}
